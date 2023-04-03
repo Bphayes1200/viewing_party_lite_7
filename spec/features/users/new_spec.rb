08634f84a5fb0d29f5c74095d0612214ec4750f0
Brian Hayes <Brian.hayes1200@gmail.com>
2023-04-03 15:17:57 -0600

    Feat: add password functionality to user creation

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -7,14 +7,18 @@ RSpec.describe "'/register'", type: :feature do
     end
 
     it "Should have a form to register a new user" do
+
       expect(page).to have_content("Viewing Party")
       expect(page).to have_content("Register a New User")
       expect(page).to have_field(:name)
       expect(page).to have_field(:email)
+      expect(page).to have_field(:password)
+      expect(page).to have_field(:password_confirmation)
       expect(page).to have_button("Create New User")
-
       fill_in :name, with: "David"
       fill_in :email, with: "David@example.com"
+      fill_in :password, with: "Password!"
+      fill_in :password_confirmation, with: "Password!"
       click_button("Create New User")
 
       expect(current_path).to eq("/users/#{User.last.id}")
--- test/factories/users.rb
+++ test/factories/users.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
     email { Faker::Internet.email  }
+    password_digest { "Password123 "}
+    password_confirmation { "Password123 "}
   end
 end
